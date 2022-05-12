ceca4965f016be8bba418b8307c7b748424819bf
John Mercouris <john@mercouris.email>
2022-05-11 23:18:53 -0500

    shell.nix: update to support changes in NixOS

--- build-scripts/shell.nix
+++ build-scripts/shell.nix
@@ -1,7 +1,9 @@
 # SPDX-FileCopyrightText: Atlas Engineer LLC
 # SPDX-License-Identifier: BSD-3-Clause
 
-# This file is meant to be used with the nyxt/gi-gtk system
+# This file is meant to be used with the nyxt/gi-gtk system. Use this
+# file to open a nix shell with the required dependencies to run SBCL
+# and load nyxt/gi-gtk.
 
 { pkgs ? import <nixpkgs> {} } :
 with builtins;
@@ -17,7 +19,8 @@ stdenv.mkDerivation {
   ];
 
   buildInputs = [
-    pkgs.gobjectIntrospection
+    pkgs.gobject-introspection
+    pkgs.pkg-config
     pkgs.enchant.out
     pkgs.gsettings-desktop-schemas.out
     pkgs.glib-networking.out
@@ -37,31 +40,19 @@ stdenv.mkDerivation {
       gst-libav
   ]);
 
-  propogatedBuildInputs = [
-    pkgs.enchant.out
-    pkgs.gsettings-desktop-schemas.out
-    pkgs.glib-networking.out
-    pkgs.pango.out
-    pkgs.cairo.out
-    pkgs.gdk-pixbuf.out
-    pkgs.gtk3.out
-    pkgs.glib.out
-    pkgs.libfixposix.out
-    pkgs.webkitgtk
-  ];
-
-  LD_LIBRARY_PATH = with stdenv.lib; "${makeLibraryPath [ pkgs.gsettings-desktop-schemas.out
-                                                          pkgs.enchant.out
-                                                          pkgs.glib-networking.out
-                                                          pkgs.webkitgtk
-                                                          pkgs.gobjectIntrospection
-                                                          pkgs.gtk3
-                                                          pkgs.pango.out
-                                                          pkgs.cairo.out
-                                                          pkgs.gdk-pixbuf.out
-                                                          pkgs.glib.out
-                                                          pkgs.libfixposix.out
-                                                          pkgs.libressl.out ]};";
+  LD_LIBRARY_PATH = with lib; "${makeLibraryPath [ pkgs.gsettings-desktop-schemas.out
+                                                   pkgs.enchant.out
+                                                   pkgs.glib-networking.out
+                                                   pkgs.webkitgtk
+                                                   pkgs.gobject-introspection
+                                                   pkgs.pkg-config.out
+                                                   pkgs.gtk3
+                                                   pkgs.pango.out
+                                                   pkgs.cairo.out
+                                                   pkgs.gdk-pixbuf.out
+                                                   pkgs.glib.out
+                                                   pkgs.libfixposix.out
+                                                   pkgs.libressl.out ]};";
 
   GIO_MODULE_DIR = "${pkgs.glib-networking.out}/lib/gio/modules/";
   GIO_EXTRA_MODULES = "${pkgs.glib-networking.out}/lib/gio/modules/";
